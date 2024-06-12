17d42ec8e8c737179bce20f7a1e3f98cc88ef546
swathikajekar <168705137+swathikajekar@users.noreply.github.com>
2024-06-12 15:46:58 +0530

    Create http.py

---
+++ http.py
@@ -0,0 +1,12 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}')
+    httpd.serve_forever()
+
+# Example usage
+if __name__ == "__main__":
+    run()
+
